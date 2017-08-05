03289717c149a9826e26f3a0d9ed87984133d2c2
Anna <anna@Big-iMac.local>
2017-08-05 01:14:07 -0400

    datasupplier improved, store data sorted and returns flexible batches

--- data.py
+++ data.py
@@ -7,25 +7,27 @@ import random
 import re
 import glob
 import numpy as np
+from collections import defaultdict
 from PIL import Image, ImageOps
 
 
 class Data_Supplier(object):
-    """docstring forSupplier."""
+    """Data Supplier. We want data, and we want it now and in order
+
+    Class to index and supply frames in a consisten maner. The frames must be
+    supplied in order. This class will allow us to get that data in a flexible
+    manner
+
+    """
 
     def __init__(self,
                  path=None,
-                 extensions=('JPG'),
                  batch_size=300,
-                 encoder=None,
-                 ignore=None):
+                 encoder=None):
 
         # Init
-        self.extensions = extensions
-        self.ignore = ignore
         self.BATCH_SIZE = batch_size
         self.batches = 0
-
         self.encoder = encoder
         self._training_dataset = list()
         self._index = 0
@@ -33,9 +35,15 @@ class Data_Supplier(object):
         if path:
             self.indexing_data(path)
 
-    def next_batch(self):
+    def next_batch(self, sequence = 24, step=1):
         """Return next batch of training data.
 
+        Default return a batch all 24 frames in 1 second of animation
+        125 X 125 X sequence
+        sequence  : int
+            Number of animations we want to fetch
+        step : int
+            Skip frames
 
         Returns
         -------
@@ -43,18 +51,31 @@ class Data_Supplier(object):
         """
         if self.encoder:
             next_batch = list()
+            group = list()
             training_batch = self._training_dataset[self._index *
-                                                    self.BATCH_SIZE:(self._index + 1) * 
-                                                    self.BATCH_SIZE]
-            for training in training_batch:
-                next_batch.append(self.encoder(training))
-            self._index += 1
-            # redimension
+                                                    self.BATCH_SIZE *
+                                                    sequence *
+                                                    step:(self._index + 1) *
+                                                    self.BATCH_SIZE *
+                                                    sequence *
+                                                    step]
+
+            for training in range(training_batch, step):
+                if len(group) < sequence:
+                    group.append(self.encoder(training))
+                    self._index += step
+                else:
+                    next_batch.append(np.array(group))
+                    group = list()
+
+                # redimension
             next_batch = np.array(next_batch)
             return next_batch.reshape((next_batch.shape[0], 1) + next_batch.shape[1:])
 
+
+
     def indexing_data(self, path):
-        """Walk the source folder and select potential photos by extension.
+        """Walk the source folder and index frames.
         Parameters
         ----------
         source_path : str
@@ -66,17 +87,26 @@ class Data_Supplier(object):
         # use endswith , ignore must be a tuple then
         # if ignore and dirpath.endswith(ignore):
         # for duplication, at the end cll the same funciton
-
+        buffer_temp = defaultdict(list)
         for (dirpath, dirnames, filenames) in os.walk(path):
             for f in filenames:
-                if f.upper().endswith(self.extensions):
+                if f.upper().endswith('JPG'):
                     # source_files.append(os.path.join(dirpath, f))
                     # ({'dir':dirpath,
                     #'filename':f,
                     #'parent_folder':parent})
                     f = os.path.join(dirpath, f)
-                    # parent = os.path.basename(os.path.normpath(dirpath))
-                    self._training_dataset.append(f)
+                    # parent directoy is the episode code
+                    parent = os.path.basename(os.path.normpath(dirpath))
+                    episode = parent.replace('_jpg','')
+                    frame = int(f.replace('frame_','').replace('.jpg',''))
+
+                    buffer_temp[episode].append({'frame':frame,'file':f})
+
+        # Sort data
+        for epis in buffer_temp:
+            sorted_epis = [ x['file'] for x in sorted(epis, key=lambda k: k['frame'])]
+            self._training_dataset.extend(sorted_epis)
 
         self.batches = int(len(self._training_dataset) / self.batch_size)
 
@@ -85,8 +115,8 @@ class Data_Supplier(object):
 
 # coders
 
-def coder_grey(file_path):
-    pixels = 125
+def coder_grey(file_path, pixels = 125):
+
     im = Image.open(file_path)
     im = ImageOps.fit(im, (pixels, pixels), Image.ANTIALIAS)
     im = ImageOps.grayscale(im)
